68112b9a96aebb797e75e9b0738741a344ad5d6b
Yurii Pasternak <10pka.muscatel@gmail.com>
2023-08-11 20:14:30 +0300

    Add GetUser feature.

---
+++ TraineeTestCsvUploader/Application/Features/UserFeatures/GetUser/GetUserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.UserFeatures.GetUser
+{
+    public class GetUserResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
--- TraineeTestCsvUploader/WebAPI/Controllers/UserController.cs
+++ TraineeTestCsvUploader/WebAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.UserFeatures.CreateUser;
 using Application.Features.UserFeatures.CreateUserCSV;
 using Application.Features.UserFeatures.DeleteUser;
+using Application.Features.UserFeatures.GetUser;
 using Application.Features.UserFeatures.GetUsers;
 using Application.Features.UserFeatures.UpdateUser;
 using MediatR;
@@ -20,21 +21,21 @@ namespace WebAPI.Controllers
         [HttpPost("CreateUserCSV")]
         public async Task<ActionResult<List<CreateUserCSVResponse>>> CreateUserCSV([FromQuery]CreateUserCSVRequest request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(request, cancellationToken);
+            return Ok(await _mediator.Send(request, cancellationToken));
         }
 
 
         [HttpPost("CreateUser")]
         public async Task<ActionResult<CreateUserResponse>> CreateUser([FromQuery] CreateUserRequest request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(request, cancellationToken);
+            return Ok(await _mediator.Send(request, cancellationToken));
         }
 
 
         [HttpPut("UpdateUser")]
         public async Task<ActionResult<UpdateUserResponse>> UpdateUser(UpdateUserRequest request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(request, cancellationToken);
+            return Ok(await _mediator.Send(request, cancellationToken));
         }
 
 
@@ -47,8 +48,13 @@ namespace WebAPI.Controllers
         [HttpGet("Users")]
         public async Task<ActionResult<List<GetUsersResponse>>> GetUsers([FromQuery] GetUsersRequest request, CancellationToken cancellationToken)
         {
-            var result = await _mediator.Send(request, cancellationToken);
-            return Ok(result);
+            return Ok(await _mediator.Send(request, cancellationToken));
+        }
+
+        [HttpGet("User")]
+        public async Task<ActionResult<GetUserResponse>> GetUser([FromQuery] GetUserRequest request, CancellationToken cancellationToken)
+        {
+            return Ok(await _mediator.Send(request, cancellationToken));
         }
 
     }
