54b3dc39fcaf2af69aee12ed4cc632426dd9c929
Yurii Pasternak <10pka.muscatel@gmail.com>
2023-08-11 20:06:23 +0300

    Add GetUsers feature.

--- TraineeTestCsvUploader/Application/Features/UserFeatures/CreateUser/CreateUserValidator.cs
+++ TraineeTestCsvUploader/Application/Features/UserFeatures/CreateUser/CreateUserValidator.cs
@@ -8,7 +8,7 @@ namespace Application.Features.UserFeatures.CreateUser
         {
             RuleFor(p => p.Name).MaximumLength(128);
             RuleFor(p => p.Phone).Matches("^[0-9]+$");
-            RuleFor(p => p.DateOfBirth).Must(x => x > new DateTime(1972, 01, 01));
+            RuleFor(p => p.DateOfBirth).Must(x => x >= new DateTime(1972, 01, 01));
             RuleFor(p => p.Salary).Must(x => x >= 0);
         }
     }
--- TraineeTestCsvUploader/WebAPI/Controllers/UserController.cs
+++ TraineeTestCsvUploader/WebAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.UserFeatures.CreateUser;
 using Application.Features.UserFeatures.CreateUserCSV;
 using Application.Features.UserFeatures.DeleteUser;
+using Application.Features.UserFeatures.GetUsers;
 using Application.Features.UserFeatures.UpdateUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -17,30 +18,37 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost("CreateUserCSV")]
-        public async Task<ActionResult<List<CreateUserCSVResponse>>> CreateUserCSV([FromQuery]CreateUserCSVRequest request)
+        public async Task<ActionResult<List<CreateUserCSVResponse>>> CreateUserCSV([FromQuery]CreateUserCSVRequest request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(request);
+            return await _mediator.Send(request, cancellationToken);
         }
 
 
         [HttpPost("CreateUser")]
-        public async Task<ActionResult<CreateUserResponse>> CreateUser([FromQuery] CreateUserRequest request)
+        public async Task<ActionResult<CreateUserResponse>> CreateUser([FromQuery] CreateUserRequest request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(request);
+            return await _mediator.Send(request, cancellationToken);
         }
 
 
         [HttpPut("UpdateUser")]
-        public async Task<ActionResult<UpdateUserResponse>> UpdateUser(UpdateUserRequest request)
+        public async Task<ActionResult<UpdateUserResponse>> UpdateUser(UpdateUserRequest request, CancellationToken cancellationToken)
         {
-            return await _mediator.Send(request);
+            return await _mediator.Send(request, cancellationToken);
         }
 
 
         [HttpDelete("DeleteUser")]
-        public async Task<ActionResult> DeleteUser(DeleteUserRequest request)
+        public async Task<ActionResult> DeleteUser(DeleteUserRequest request, CancellationToken cancellationToken)
         {
-            return Ok(await _mediator.Send(request));
+            return Ok(await _mediator.Send(request, cancellationToken));
+        }
+
+        [HttpGet("Users")]
+        public async Task<ActionResult<List<GetUsersResponse>>> GetUsers([FromQuery] GetUsersRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(request, cancellationToken);
+            return Ok(result);
         }
 
     }
