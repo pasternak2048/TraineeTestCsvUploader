5694e6c60ead1fdb009417af0b69e4dddce14536
Yurii Pasternak <10pka.muscatel@gmail.com>
2023-08-13 07:24:50 +0300

    Add Users table.

--- TraineeTestCsvUploader/WebUI/Pages/Index.cshtml.cs
+++ TraineeTestCsvUploader/WebUI/Pages/Index.cshtml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Common.Models;
+using Application.Features.UserFeatures.DeleteUser;
+using Application.Features.UserFeatures.GetUsers;
+using Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebUI.Pages
@@ -6,15 +11,34 @@ namespace WebUI.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IMediator _mediator;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IMediator mediator)
         {
             _logger = logger;
+            _mediator = mediator;
         }
 
-        public void OnGet()
-        {
+        public PaginatedList<GetUsersResponse> Users { get; set; }
 
+        public async Task OnGet(int pageNumber = 1)
+        {
+            Users = await _mediator.Send(new GetUsersRequest()
+            {
+                PageNumber = pageNumber,
+            });
+        }
+
+
+
+        public async Task<IActionResult> OnPostDelete(Guid id, int pageNumber = 1)
+        {
+            await _mediator.Send(new DeleteUserRequest()
+            {
+                Id = id
+            });
+
+            return RedirectToPage("./Index", new { pageNumber = pageNumber });
         }
     }
 }
